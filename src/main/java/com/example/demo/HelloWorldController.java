6be0c79b5e8a328c4c5d4f6041203bc614eef8c8
Nikita <nrw@narola.email>
2023-09-05 10:12:25 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController
+{
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "Hello Welcome to spring boot demo";
+    }
+}
