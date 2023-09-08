81a17bf9792d0aa3f9572f0fd7fa89ddd9215582
Nikita <nrw@narola.email>
2023-09-08 15:33:01 +0530

    RestTemplateExample

--- src/main/java/com/example/demo/PropertyPlaceholders/MyController.java
+++ src/main/java/com/example/demo/PropertyPlaceholders/MyController.java
@@ -1,23 +1,23 @@
-//package com.example.demo.PropertyPlaceholders;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class MyController {
-//
-//    private final AppConfig appConfig;
-//
-//    @Autowired
-//    public MyController(AppConfig appConfig) {
-//        this.appConfig = appConfig;
-//    }
-//
-//    @GetMapping("/app-info")
-//    public String getAppInfo() {
-//        return "Application Name: " + appConfig.getAppName() +
-//                "\nApplication Description: " + appConfig.getAppDescription();
-//    }
-//}
+package com.example.demo.PropertyPlaceholders;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    private final AppConfig appConfig;
+
+    @Autowired
+    public MyController(AppConfig appConfig) {
+        this.appConfig = appConfig;
+    }
+
+    @GetMapping("/app-info")
+    public String getAppInfo() {
+        return "Application Name: " + appConfig.getAppName() +
+                "\nApplication Description: " + appConfig.getAppDescription();
+    }
+}
