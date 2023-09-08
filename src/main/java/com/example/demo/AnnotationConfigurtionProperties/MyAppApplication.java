a3b613ad115f0d243f7663bdf4145266479a051e
Nikita <nrw@narola.email>
2023-09-08 15:34:10 +0530

    RestTemplateExample

--- src/main/java/com/example/demo/AnnotationConfigurtionProperties/MyAppApplication.java
+++ src/main/java/com/example/demo/AnnotationConfigurtionProperties/MyAppApplication.java
@@ -8,6 +8,5 @@ public class MyAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyAppApplication.class, args);
-        Hello I am nikita i like to play games.!!!
     }
 }
